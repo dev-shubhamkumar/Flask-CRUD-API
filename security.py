5057a26ba2a1d57714140cd8b3861c0ce98bcba7
Shubham Kumar <developer.shubham07@gmail.com>
2021-03-14 20:30:16 +0530

    First commit, added REST API to access items, stores and user authentication.

---
+++ security.py
@@ -0,0 +1,13 @@
+from werkzeug.security import safe_str_cmp  ##Library to compare string instead of using ==
+from models.user import UserModel
+
+
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):  ##Use of "safe string compare"
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
